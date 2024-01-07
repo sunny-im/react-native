75e66e79f15f486e0fec7dfbbbeb41bca01ee871
sunny <hellossun.dahae@gmail.com>
2024-01-08 08:39:29 +0900

    stack navigator

--- components/Detail.jsx
+++ components/Detail.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import React, {useEffect} from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { Text, List } from 'react-native-paper';
 
 const Detail = () => {
   //=========================================================
+  const [isLoading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState([]);
+  //=========================================================
+  const movie = async () => {
+    try {
+      const res = await fetch('https://reactnative.dev/movies.json');
+      const json = await res.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    movie();
+  },[])
   //=========================================================
   return (
-    <View>
-      <Text>detail page</Text>
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <List.Section>
+          {data.map(item => (
+            <List.Item 
+              title={item.title}
+              description={item.releaseYear}
+              left={props => <List.Icon {...props} icon="star-circle-outline" />}
+            />
+          ))}
+        </List.Section>
+      )}
     </View>
   )
 }
